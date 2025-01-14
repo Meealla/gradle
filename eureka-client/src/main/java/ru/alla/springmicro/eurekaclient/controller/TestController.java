65448c141ba9937b91048b7450df1468ef155ef5
Meealla <achvokina@mail.ru>
2025-01-14 23:07:49 +0300

    api gateway создан . тест завершение

--- eureka-client/src/main/java/ru/alla/springmicro/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/alla/springmicro/eurekaclient/controller/TestController.java
@@ -1,15 +1,22 @@
 package ru.alla.springmicro.eurekaclient.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
 
+    @Value("${tmpVar}")
+    private int tmpVar;
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return id + ":" + tmpVar;
     }
 }
